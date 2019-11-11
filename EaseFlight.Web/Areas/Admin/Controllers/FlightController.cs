ae95424573754b1806bf87f588eaae98e54cc7c6
Luc Nguyen <itsdevlucnguyen@gmail.com>
2019-11-11 16:51:41 +0700

    [Admin] Add create flight modal

--- EaseFlight.Web/Areas/Admin/Controllers/FlightController.cs
+++ EaseFlight.Web/Areas/Admin/Controllers/FlightController.cs
@@ -1,4 +1,6 @@
 ﻿using EaseFlight.BLL.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace EaseFlight.Web.Areas.Admin.Controllers
@@ -7,12 +9,17 @@ namespace EaseFlight.Web.Areas.Admin.Controllers
     {
         #region Properties
         private IFlightService FlightService { get; set; }
+        private IPlaneService PlaneService { get; set; }
+        private IPlaneAirportService PlaneAirportService { get; set; }
         #endregion
 
         #region Constructor
-        public FlightController(IFlightService flightService)
+        public FlightController(IFlightService flightService, IPlaneService planeService,
+            IPlaneAirportService planeAirportService)
         {
-            this.FlightService = flightService;            
+            this.FlightService = flightService;
+            this.PlaneService = planeService;
+            this.PlaneAirportService = planeAirportService;
         }
         #endregion
 
@@ -21,8 +28,22 @@ namespace EaseFlight.Web.Areas.Admin.Controllers
         public ActionResult Index()
         {
             ViewData["flights"] = this.FlightService.FindAll();
+            ViewData["planes"] = this.PlaneService.FindAll().Where(plane => plane.PlaneAirports.Count > 1);
 
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetAirport(string planeId)
+        {
+            var result = new JsonResult { ContentType = "text" };
+            var airports = this.PlaneAirportService.FindByPlane(int.Parse(planeId)).ToList();
+            var data = new List<string>();
+
+            airports.ForEach(air => data.Add(air.AirportID.ToString() + ":" + air.Airport.Name));
+            result.Data = new { airport = data};
+
+            return result;
         }
         #endregion
     }
--- EaseFlight.Web/Scripts/admin/pages/flight.js
+++ EaseFlight.Web/Scripts/admin/pages/flight.js
@@ -25,5 +25,38 @@
             "edit": { name: "Edit", icon: "edit" },
             "delete": { name: "Delete", icon: "delete" }
         }
-    });   
+    });  
+
+    //Select plane onchange
+    $('select[name="plane"]').change(function () {
+        planeSelectOnchange($(this).val());
+    });
+
+    planeSelectOnchange($('select[name="plane"]').val());
 })
+
+function planeSelectOnchange(planeId) {
+    $.ajax({
+        url: '/Admin/Flight/GetAirport',
+        type: 'post',
+        data: {
+            planeId: planeId
+        },
+        success: function (response) {
+            var data = JSON.parse(response).airport;
+            var depart = $('select[name="depart"]');
+            var arrival = $('select[name="arrival"]');
+
+            depart.empty(); arrival.empty();
+
+            for (var i = 0; i < data.length; ++i) {
+                var airport = data[i].split(':');
+                depart.append('<option value="' + airport[0] + '">' + airport[1] + '</option>');
+                arrival.append('<option value="' + airport[0] + '">' + airport[1] + '</option>');
+            }
+
+            depart.trigger('change');
+            arrival.trigger('change');
+        }
+    });
+}
