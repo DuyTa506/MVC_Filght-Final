07f52e67b489c729a4ffb97a9049a80bbbf73f67
Luc Nguyen <itsdevlucnguyen@gmail.com>
2019-11-19 10:38:49 +0700

    [Admin] Change route default for all controller

--- EaseFlight.Web/Areas/Admin/AdminAreaRegistration.cs
+++ EaseFlight.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace EaseFlight.Web.Areas.Admin
 {
@@ -14,6 +15,22 @@ namespace EaseFlight.Web.Areas.Admin
 
         public override void RegisterArea(AreaRegistrationContext context) 
         {
+            var controllerAdmin = new List<string> { "Account", "Airport", "Country", "Flight", "Home", "Plane", "Ticket" };
+
+            //Set default route for all controller
+            foreach(var ctroller in controllerAdmin)
+            {
+                var name = "Admin_"+ ctroller +"_Default";
+                var url = "Admin/"+ ctroller +"/{action}/{id}";
+
+                context.MapRoute(name, url , new
+                {
+                    controller = ctroller,
+                    action = "Index",
+                    id = UrlParameter.Optional,
+                });
+            }
+
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
